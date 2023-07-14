430c8b4ac7b0eb9cb306d84183243ccfb473b01e
Pablo <pabloag43@educastur.es>
2023-07-14 11:22:02 +0200

    Realizamos el proceso de autenticación del usuario

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,6 +3,7 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -11,6 +12,11 @@ export class AuthController {
   @Post()
   create(@Body() createuserDto: CreateUserDto) {
     return this.authService.create(createuserDto);
+  }
+
+  @Post('/login')
+  login(@Body() loginDto: LoginDto) {
+    return this.authService.login(loginDto);
   }
 
   @Get()
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,71 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+import * as bcryptjs from 'bcryptjs'
+
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
+import { User } from './entities/user.entity';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
-  create(createUserDto: CreateUserDto) {
-    console.log(createUserDto)
-    return 'This action adds a new auth';
+
+  constructor(
+    @InjectModel(User.name) private userModel: Model<User>
+  ) { }
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+
+
+    try {
+      const { password, ...userData } = createUserDto;
+
+      // encriptamos la contraseña
+      const newUser = new this.userModel({
+        password: bcryptjs.hashSync(password, 10),
+        ...userData
+      });
+
+      await newUser.save();
+      const { password: _, ...user } = newUser.toJSON();
+
+      //TODO: guardar el usuario
+
+
+
+
+      return user;
+    }
+    catch (error) {
+      if (error.code === 11000)
+        throw new BadRequestException(`${createUserDto.email} ya existe`)
+      throw new InternalServerErrorException('Algo malo ha ocurrido')
+    }
+
+  }
+
+  // metodo que usaremos para intentar loggear al usuario
+  async login(loginDto: LoginDto) {
+    // obtenemos la contraseña y el correo del usuario
+    const { email, password } = loginDto;
+
+    // almacenamos el correo del usuario
+    const user = await this.userModel.findOne({ email })
+
+    // verificamos que el correo del usuario este en la base de datos
+    if (!user) throw new UnauthorizedException('Datos del usuario incorrectos - email')
+
+    // verificamos que la contraseña del usuario este en la base de datos
+    if (!bcryptjs.compareSync(password, user.password)) throw new UnauthorizedException('Datos del usuario incorrectos - password')
+
+    const { password: _, ...rest } = user.toJSON();
+
+    return {
+      ...rest,
+      token: '123',
+    }
   }
 
   findAll() {
